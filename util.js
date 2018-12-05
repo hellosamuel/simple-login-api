f854cf06f675948d3e126280cd1b1eda0033008c
hellosamuel <samuelis419@gmail.com>
2018-12-05 14:16:47 +0900

    ADD util.js

---
+++ util.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'MySuperSecretKey';
+
+let util = {};
+
+util.successTrue = function(data) {
+  return {
+    success:true,
+    message:null,
+    errors:null,
+    data:data
+  };
+};
+
+util.successFalse = function(err, message) {
+  if(!err && !message) message = 'data not found';
+  return {
+    success:false,
+    message:message,
+    errors:(err)? util.parseError(err): null,
+    data:null
+  };
+};
+
+util.parseError = function(errors) {
+  let parsed = {};
+  if(errors.name == 'ValidationError'){
+    for(let name in errors.errors){
+      let validationError = errors.errors[name];
+      parsed[name] = { message:validationError.message };
+    }
+  } else if(errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
+    parsed.username = { message:'This username already exists!' };
+  } else {
+    parsed.unhandled = errors;
+  }
+  return parsed;
+};
+
+
+// middlewares
+util.isLoggedin = function(req, res, next) {
+  const token = req.headers['x-access-token'];
+  if (!token) return res.json(util.successFalse(null,'token is required!'));
+  else {
+    jwt.verify(token, JWT_SECRET, function(err, decoded) {
+      if(err) return res.json(util.successFalse(err));
+      else{
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+};
+
+module.exports = util;
